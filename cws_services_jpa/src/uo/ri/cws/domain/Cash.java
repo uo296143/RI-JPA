6ad8862a3e6ff7e6d722deb165e982765970dd4b
uo296143 <uo296143@uniovi.es>
2025-10-22 12:59:07 +0200

    Cambios hechos en el formateo del codigo

--- cws_services_jpa/src/uo/ri/cws/domain/Cash.java
+++ cws_services_jpa/src/uo/ri/cws/domain/Cash.java
@@ -1,5 +1,7 @@
 package uo.ri.cws.domain;
 
+import uo.ri.util.assertion.ArgumentChecks;
+
 public class Cash extends PaymentMean {
 
 	
@@ -9,6 +11,11 @@ public class Cash extends PaymentMean {
 	@Override
 	public boolean canPay(Double amount) {
 		return true;
+	}
+
+	public Cash(Client client) {
+		ArgumentChecks.isNotNull(client, "invalid null client");
+		Associations.Holds.link(this, client);
 	}
 
 
